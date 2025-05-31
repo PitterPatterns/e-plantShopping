1b2b17a26a62dd20397c0dc47d7448d339c5422b
PitterPatterns <pitteralec@yahoo.com>
2025-05-31 12:53:13 -0500

    update

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,20 +9,39 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    const calculateTotal = (cart) => {
+      let total = 0;
+    
+      cart.forEach(item => {
+        const quantity = item.quantity;
+        const cost = parseFloat(item.cost.substring(1)); // Remove '$' and convert to number
+        total += quantity * cost;
+      });
+    
+      
+    };
+  
   };
 
   const handleContinueShopping = (e) => {
+    onContinueShopping();
    
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity(({ name: item.name, quantity: item.quantity + 1 }))
+    )
   };
 
   const handleDecrement = (item) => {
-   
+    {item.quantity > 1 ? (
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }))
+    ) : (
+      dispatch(removeItem({ name: item.name, quantity: item.quantity }))
+    )} 
+    
   };
 
   const handleRemove = (item) => {
